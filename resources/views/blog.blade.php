5e037e03c6ac34a33a8728014b97fe310ad0dae6
Farhan Fitran Maulana <farhanfitran123@gmail.com>
2026-04-20 12:12:24 +0700

    pertemuan 7

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tutorial Laravel - www.malasngoding.com</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Oswald:wght@600;700&family=Lato:ital,wght@0,300;0,400;0,700;1,400&display=swap">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class ="container">
+        <h3>www.malasngoding.com</h3>
+        <p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+        <p>Ini adalah view blog. ada di route blog.</p>
+    </div>
+</body>
+
+</html>
