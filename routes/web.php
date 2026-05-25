1e79004c7c08b6aab5b57a5d97d874fc884f0b3a
Farhan Fitran Maulana <farhanfitran123@gmail.com>
2026-05-25 12:07:32 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/halo', function () {
 
 Route::get('blog', function () {
 	return view('blog');
-    
+
 });
 
 Route::get('dosen', [DosenController::class, 'index']);
@@ -67,3 +67,11 @@ Route::get('/blog/kontak', [BlogController::class, 'kontak']);
 
 // CRUD Tabel Pegawai
 Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
